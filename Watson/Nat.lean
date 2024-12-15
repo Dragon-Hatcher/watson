d2be9ddbe90491774751f148203240214516408c
Dragon-Hatcher <danieldragonhatcher@gmail.com>
2024-12-15 12:32:30 -0800

    Chp. 2.1

---
+++ Watson/Nat.lean
@@ -0,0 +1,36 @@
+namespace Watson
+
+open _root_ renaming Nat → LeanNat
+
+/--
+The integers greater than or equal to zero. Defined in terms of zero and a
+successor function.
+-/
+inductive Nat where
+  | zero : Nat
+  | succ (n : Nat) : Nat
+
+
+/-! Test -/
+-- Here we make it so we can use integer literals for instances of our custom
+-- `Nat` type.
+
+def ofNat : (n : LeanNat) → Nat
+  | LeanNat.zero   => Nat.zero
+  | LeanNat.succ m => Nat.succ (ofNat m)
+
+instance (n : LeanNat) : OfNat Nat n where
+  ofNat := ofNat n
+
+@[simp]
+theorem zero_eq_lit : Nat.zero = 0 := rfl
+
+
+theorem succ_neq_zero {n : Nat} : n.succ ≠ 0 :=
+  Nat.noConfusion
+
+theorem succ_inj {a b : Nat} : a.succ = b.succ ↔ a = b :=
+  (Nat.succ.injEq a b).to_iff
+
+
+end Watson

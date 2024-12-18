79a0af8acfbb16f3692b5f0e95fa5ad8fe9270c2
Dragon-Hatcher <danieldragonhatcher@gmail.com>
2024-12-17 19:42:28 -0800

    Section 2.2 complete!

--- Watson/Nat.lean
+++ Watson/Nat.lean
@@ -12,6 +12,7 @@ inductive Nat where
   | zero : Nat
   | succ (n : Nat) : Nat
 
+def Nat.one : Nat := succ zero
 
 /-! Test -/
 -- Here we make it so we can use integer literals for instances of our custom
@@ -28,7 +29,7 @@ instance (n : LeanNat) : OfNat Nat n where
 theorem Nat.zero_eq_lit : Nat.zero = 0 := rfl
 
 
-theorem Nat.succ_neq_zero {n : Nat} : n.succ ≠ 0 :=
+theorem Nat.succ_ne_zero {n : Nat} : n.succ ≠ 0 :=
   Nat.noConfusion
 
 theorem Nat.succ_inj {a b : Nat} : a.succ = b.succ ↔ a = b :=
@@ -36,29 +37,32 @@ theorem Nat.succ_inj {a b : Nat} : a.succ = b.succ ↔ a = b :=
 
 
 def Nat.add : (a b : Nat) → Nat
-  | zero,    b => b
-  | succ a', b => Nat.succ (Nat.add a' b)
+  | a, zero    => a
+  | a, succ b' => Nat.succ (Nat.add a b')
 
 instance : Add Nat where
   add := Nat.add
 
 @[simp]
-theorem Nat.zero_add (n : Nat) : 0 + n = n := rfl
+theorem Nat.add_zero (n : Nat) : n + 0 = n := rfl
 
 @[simp]
-theorem Nat.succ_add (n m : Nat) : n.succ + m = (n + m).succ := rfl
+theorem Nat.succ_eq_add_one (n : Nat) : n.succ = n + 1 := rfl
 
 @[simp]
-theorem Nat.add_zero (n : Nat) : n + 0 = n := by
+theorem Nat.add_succ (n m : Nat) : n + m.succ = (n + m).succ := rfl
+
+@[simp]
+theorem Nat.zero_add (n : Nat) : 0 + n = n := by
   induction n with
   | zero => rfl
-  | succ n' ih => rw [succ_add, ih]
+  | succ n' ih => rw [add_succ, ih]
 
 @[simp]
-theorem Nat.add_succ (n m : Nat) : n + m.succ = (n + m).succ := by
-  induction n with
+theorem Nat.succ_add (n m : Nat) : n.succ + m = (n + m).succ := by
+  induction m with
   | zero => simp
-  | succ n' ih => rw [succ_add, ih]; rfl
+  | succ n' ih => rw [add_succ, ih]; rfl
 
 @[simp]
 theorem Nat.add_comm (n m : Nat) : n + m = m + n := by
@@ -73,12 +77,22 @@ theorem Nat.add_assoc (a b c : Nat) : (a + b) + c = a + (b + c) := by
   | succ n' ih => rw [add_succ, add_succ, add_succ, ih]
 
 @[simp]
-theorem Nat.add_cancels (a b c : Nat) : a + b = a + c → b = c := by
+theorem Nat.add_cancels {a b c : Nat} : a + b = a + c → b = c := by
   induction a with
   | zero => simp
   | succ a' ih =>
       rw [succ_add, succ_add, succ_inj]
       assumption
+
+theorem Nat.ne_add_one (n : Nat) : n ≠ n + 1 := by
+  induction n with
+  | zero => exact succ_ne_zero.symm
+  | succ n' =>
+      intro h
+      have hh := succ_inj.mp h
+      contradiction
+
+theorem Nat.add_one_ne (n : Nat) : n + 1 ≠ n := (ne_add_one n).symm
 
 
 def Nat.is_pos (n : Nat) := n ≠ 0
@@ -89,9 +103,9 @@ theorem Nat.pos_add_is_pos (a b : Nat) : a.is_pos → (a + b).is_pos := by
   | zero => simp; assumption
   | succ b' ih =>
       simp
-      exact Nat.succ_neq_zero
+      exact Nat.succ_ne_zero
 
-theorem Nat.sum_zero_not_pos (a b : Nat) : a + b = 0 → a = 0 ∧ b = 0 := by
+theorem Nat.sum_zero_not_pos {a b : Nat} : a + b = 0 → a = 0 ∧ b = 0 := by
   intro h
   constructor
   . by_contra! ha
@@ -106,5 +120,157 @@ theorem Nat.exists_pred (a : Nat) (h : a.is_pos) : ∃ b : Nat, b.succ = a := by
   induction a with
   | zero => contradiction
   | succ a' ih => use a'
+
+theorem Nat.ne_add_pos {a b : Nat} (hb : b.is_pos) : a ≠ a + b := by
+  intro h
+  nth_rw 1 [← add_zero a] at h
+  have hbn := (add_cancels h).symm
+  contradiction
+
+
+def Nat.le (n m : Nat) := ∃ a, m = n + a
+def Nat.lt (n m : Nat) := ∃ a, m = n + a ∧ a.is_pos
+
+instance : LE Nat where
+  le := Nat.le
+
+instance : LT Nat where
+  lt := Nat.lt
+
+theorem Nat.not_lt_zero (n : Nat) : ¬(n < 0) := by
+  intro ⟨diff, ⟨h_diff, diff_pos⟩⟩
+  have _ : diff = 0 := (sum_zero_not_pos h_diff.symm).right
+  contradiction
+
+@[simp]
+theorem Nat.zero_le (n : Nat) : 0 ≤ n := ⟨n, (zero_add n).symm⟩
+
+theorem Nat.le_zero_eq_zero (n : Nat) : n ≤ 0 → n = 0 := by
+  intro ⟨diff, h_diff⟩
+  exact (sum_zero_not_pos h_diff.symm).left
+
+theorem Nat.lt_is_le {a b : Nat} : a < b → a ≤ b := by
+  intro ⟨n, ⟨h, _⟩⟩
+  exact ⟨n, h⟩
+
+theorem Nat.le_rfl (a : Nat) : a ≤ a :=
+  ⟨0, by simp⟩
+
+theorem Nat.le_trans {a b c : Nat} (hab : a ≤ b) (hbc : b ≤ c) : a ≤ c := by
+  have ⟨b_min_a, h_b_min_a⟩ := hab
+  have ⟨c_min_b, h_c_min_b⟩ := hbc
+  rw [h_b_min_a, add_assoc] at h_c_min_b
+  use b_min_a + c_min_b
+
+theorem Nat.lt_le_is_lt {a b c : Nat} (hab : a < b) (hbc : b ≤ c) : a < c := by
+  have ⟨b_min_a, ⟨h_b_min_a, b_min_a_pos⟩⟩ := hab
+  have ⟨c_min_b, h_c_min_b⟩ := hbc
+  rw [h_b_min_a, add_assoc] at h_c_min_b
+  use b_min_a + c_min_b
+  exact ⟨h_c_min_b, pos_add_is_pos _ _ b_min_a_pos⟩
+
+theorem Nat.lt_trans {a b c : Nat} (hab : a < b) (hbc : b < c) : a < c := by
+  have hbc := lt_is_le hbc
+  exact lt_le_is_lt hab hbc
+
+theorem Nat.le_anti_symm {a b : Nat} (hab : a ≤ b) (hba : b ≤ a) : a = b := by
+  have ⟨b_min_a, h_b_min_a⟩ := hba
+  have ⟨a_min_b, h_a_min_b⟩ := hab
+  rw [h_b_min_a, add_assoc] at h_a_min_b
+  nth_rw 1 [← add_zero b] at h_a_min_b
+  have ⟨hb₀, ha₀⟩ := sum_zero_not_pos (add_cancels h_a_min_b).symm
+  rw [h_b_min_a, hb₀, add_zero]
+
+theorem Nat.le_iff_le_add {a b c : Nat} : a ≤ b ↔ a + c ≤ b + c := by
+  constructor
+  · intro ⟨b_min_a, h_b_min_a⟩
+    use b_min_a
+    rw [h_b_min_a]
+    rw [add_assoc, add_comm b_min_a, ← add_assoc]
+  intro ⟨b_min_a, h_b_min_a⟩
+  rw [add_comm, add_comm a, add_assoc] at h_b_min_a
+  have h_b_min_a := add_cancels h_b_min_a
+  use b_min_a
+
+theorem Nat.lt_succ (a : Nat) : a < a.succ := by
+  use 1
+  exact ⟨rfl, succ_ne_zero⟩
+
+theorem Nat.le_succ (a : Nat) : a ≤ a.succ := lt_is_le (lt_succ a)
+
+theorem Nat.lt_iff_succ_le {a b : Nat} : a < b ↔ a.succ ≤ b := by
+  constructor
+  · intro ⟨diff, ⟨h_diff, diff_pos⟩⟩
+    have ⟨diff', h_diff'⟩ := exists_pred diff diff_pos
+    rw [← h_diff', add_succ, ← succ_add] at h_diff
+    exact ⟨diff', h_diff⟩
+  intro ⟨diff, h_diff⟩
+  rw [succ_eq_add_one, add_assoc] at h_diff
+  use 1 + diff
+  exact ⟨h_diff, by rw [add_comm]; exact succ_ne_zero⟩
+
+theorem Nat.lt_succ_iff_le {a b : Nat} : a < b.succ ↔ a ≤ b := by
+  constructor
+  · intro ⟨diff, ⟨h_diff, diff_pos⟩⟩
+    have ⟨diff', h_diff'⟩ := exists_pred diff diff_pos
+    rw [← h_diff', add_succ, succ_inj] at h_diff
+    exact ⟨diff', h_diff⟩
+  intro ⟨diff, h_diff⟩
+  rw [h_diff, ← add_succ]
+  use diff.succ
+  exact ⟨rfl, succ_ne_zero⟩
+
+theorem Nat.le_lt_or_eq {a b : Nat} (h : a ≤ b) : a < b ∨ a = b := by
+  have ⟨diff, h_diff⟩ := h
+  cases diff with
+  | zero       => right; simp at h_diff; exact h_diff.symm
+  | succ diff' => left; exact ⟨diff'.succ, ⟨h_diff, succ_ne_zero⟩⟩
+
+theorem Nat.lt_trichotomy (a b : Nat) : a < b ∨ a = b ∨ a > b := by
+  induction a with
+  | zero       => cases b with
+    | zero     => right; left; rfl
+    | succ b'  => left; use b'.succ; exact ⟨by simp, succ_ne_zero⟩
+  | succ a' ih =>
+      rcases ih with h | h | h
+      · have hle : a'.succ ≤ b := lt_iff_succ_le.mp h
+        cases le_lt_or_eq hle
+        · left; assumption
+        · right; left; assumption
+      · right; right;
+        exact ⟨1, ⟨by rw [h]; rfl, succ_ne_zero⟩⟩
+      right; right;
+      have ha : a'.succ > a' := lt_succ a'
+      exact lt_trans h ha
+
+
+theorem Nat.strong_induction' (motive : Nat → Prop) (m₀ : Nat)
+  (h : ∀ m ≥ m₀, (∀ m', m₀ ≤ m' ∧ m' < m → motive m') → motive m)
+  : ∀ m ≥ m₀, motive m := by
+  intro m h_m_ge_m₀
+  have ha : ∀ n, ∀ m', m₀ ≤ m' ∧ m' < n → motive m' := by
+    intro n
+    induction n with
+    | zero =>
+        intro m' ⟨_, m'_lt_0⟩
+        absurd m'_lt_0
+        exact not_lt_zero m'
+    | succ n' ih =>
+        intro m' ⟨m₀_le_m', m'_lt_n'_succ⟩
+        apply h m' m₀_le_m'
+        intro m'' ⟨m₀_le_m'', m''_lt_m'⟩
+        apply ih
+        have m'_le_n' := lt_succ_iff_le.mp m'_lt_n'_succ
+        exact ⟨m₀_le_m'', lt_le_is_lt m''_lt_m' m'_le_n'⟩
+  exact h m h_m_ge_m₀ (ha m)
+
+theorem Nat.strong_induction (motive : Nat → Prop)
+  (h : ∀ m, (∀ m' < m, motive m') → motive m)
+  : ∀ m, motive m := by
+  have h' : ∀ m ≥ 0, (∀ m', 0 ≤ m' ∧ m' < m → motive m') → motive m := by
+    simp; assumption
+  have ha := strong_induction' motive 0 h'
+  simp at ha; assumption
+
 
 end Watson
--- Watson/Tests/TTAnalysis/Chapter2.lean
+++ Watson/Tests/TTAnalysis/Chapter2.lean
@@ -19,10 +19,10 @@ theorem prop_2_1_4 : ∃ n : Nat, n = 3 := by
 
 theorem axiom_2_3 : ∀ n : Nat, n.succ ≠ 0 := by
   intro n
-  exact Nat.succ_neq_zero
+  exact Nat.succ_ne_zero
 
 theorem prop_2_1_6 : (4 : Nat) ≠ 0 := by
-  exact Nat.succ_neq_zero
+  exact Nat.succ_ne_zero
 
 theorem axiom_2_4 {n m : Nat} : n ≠ m → n.succ ≠ m.succ := by
   contrapose!
@@ -31,7 +31,7 @@ theorem axiom_2_4 {n m : Nat} : n ≠ m → n.succ ≠ m.succ := by
 theorem prop_2_1_8 : (6 : Nat) ≠ 2 := by
   apply axiom_2_4
   apply axiom_2_4
-  exact Nat.succ_neq_zero
+  exact Nat.succ_ne_zero
 
 theorem axiom_2_5 {motive : Nat → Prop}
   (zero : motive Nat.zero) (succ : (n : Nat) → motive n → motive n.succ)
@@ -41,5 +41,51 @@ theorem axiom_2_5 {motive : Nat → Prop}
 
 
 /-! # Section 2.2 -/
+
+variable (a b c n m : Nat)
+
+theorem lemma_2_2_2 : n + 0 = n := Nat.add_zero n
+
+theorem lemma_2_2_3 : n.succ + m = (n + m).succ := Nat.succ_add n m
+
+theorem prop_2_2_4 : a + b = b + a := Nat.add_comm a b
+
+theorem prop_2_2_5 : (a + b) + c = a + (b + c) :=  Nat.add_assoc a b c
+
+theorem prop_2_2_6 : a + b = a + c → b = c := Nat.add_cancels
+
+theorem prop_2_2_8 : a.is_pos → (a + b).is_pos := Nat.pos_add_is_pos a b
+
+theorem lemma_2_2_10 : a.is_pos → ∃ b : Nat, b.succ = a := Nat.exists_pred a
+
+theorem prop_2_2_12_a : a ≤ a := Nat.le_rfl a
+theorem prop_2_2_12_b : a ≤ b → b ≤ c → a ≤ c := Nat.le_trans
+theorem prop_2_2_12_c : a ≤ b → b ≤ a → a = b := Nat.le_anti_symm
+theorem prop_2_2_12_d : a ≤ b ↔ a + c ≤ b + c := Nat.le_iff_le_add
+theorem prop_2_2_12_e : a < b ↔ a.succ ≤ b := Nat.lt_iff_succ_le
+theorem prop_2_2_12_f : a < b ↔ ∃ c, b = a + c ∧ c.is_pos := Eq.to_iff rfl
+
+theorem prop_2_2_13 : a < b ∨ a = b ∨ a > b := Nat.lt_trichotomy a b
+
+theorem prop_2_2_14 (motive : Nat → Prop) (m₀ : Nat)
+  (h : ∀ m ≥ m₀, (∀ m', m₀ ≤ m' ∧ m' < m → motive m') → motive m)
+  : ∀ m ≥ m₀, motive m := Nat.strong_induction' motive m₀ h
+
+theorem ex_2_2_6 (motive : Nat → Prop) (n : Nat)
+  (h: ∀ m, motive m.succ → motive m) (hn : motive n)
+  : ∀ m ≤ n, motive m := by
+  induction n with
+  | zero       =>
+      intro m m_le_zero
+      rw [Nat.le_zero_eq_zero m m_le_zero]
+      assumption
+  | succ n' ih =>
+      intro m m_le_n'_succ
+      have hn' : motive n' := h n' hn
+      cases Nat.le_lt_or_eq m_le_n'_succ with
+      | inr m_eq_n'      => rw [m_eq_n']; assumption
+      | inl m_lt_n'_succ =>
+          have m_le_n' : m ≤ n' := Nat.lt_succ_iff_le.mp m_lt_n'_succ
+          exact ih hn' m m_le_n'
 
 end Watson

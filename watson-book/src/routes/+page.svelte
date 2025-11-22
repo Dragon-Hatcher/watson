6e6a7d6b8f002df161ed6629921b12a6a59abeb7
Daniel James <danieldragonhatcher@gmail.com>
2025-11-21 18:09:29 -0800

    Book wip

--- watson-book/src/routes/+page.svelte
+++ watson-book/src/routes/+page.svelte
@@ -20,6 +20,70 @@
 	></script>
 </svelte:head>
 
+<aside class="nav">
+    <div class="section">I: Foundations</div>
+
+    <ol class="chapter-list">
+        <li>
+            <div class="chapter"><span class="num">1</span> Logic</div>
+            <ol class="part-list">
+                <li class="part"><div><span class="num">1.1</span> Section the First</div></li>
+                <li class="part selected"><div><span class="num">1.2</span> Section the Second</div></li>
+                <li class="part"><div><span class="num">1.3</span> Section the Third</div></li>
+            </ol>
+        </li>
+        <li>
+            <div class="chapter"><span class="num">2</span> Set Theory</div>
+            <ol class="part-list">
+                <li class="part"><div><span class="num">2.1</span> Section the First</div></li>
+                <li class="part"><div><span class="num">2.2</span> Section the Second</div></li>
+                <li class="part"><div><span class="num">2.3</span> Section the Third</div></li>
+            </ol>
+        </li>
+        <li>
+            <div class="chapter"><span class="num">3</span> Numbers</div>
+            <ol class="part-list">
+                <li class="part"><div><span class="num">3.1</span> The Integers</div></li>
+                <li class="part"><div><span class="num">3.2</span> The Rationals</div></li>
+                <li class="part"><div><span class="num">3.3</span> The Reals</div></li>
+            </ol>
+        </li>
+    </ol>
+
+    <div class="section">II: Abstract Algebra</div>
+
+    <ol class="chapter-list">
+        <li>
+            <div class="chapter"><span class="num">4</span> Groups</div>
+            <ol class="part-list">
+                <li class="part"><div><span class="num">4.1</span> Definitions</div></li>
+                <li class="part"><div><span class="num">4.2</span> Subgroups</div></li>
+                <li class="part"><div><span class="num">4.3</span> Homomorphisms and Isomorphisms</div></li>
+            </ol>
+        </li>
+        <li>
+            <div class="chapter"><span class="num">5</span> Rings</div>
+            <ol class="part-list">
+                <li class="part"><div><span class="num">5.1</span> Ring Structure</div></li>
+                <li class="part"><div><span class="num">5.2</span> Ideals and Quotient Rings</div></li>
+                <li class="part"><div><span class="num">5.3</span> Ring Homomorphisms</div></li>
+            </ol>
+        </li>
+        <li>
+            <div class="chapter"><span class="num">6</span> Fields</div>
+            <ol class="part-list">
+                <li class="part"><div><span class="num">6.1</span> Field Extensions</div></li>
+                <li class="part"><div><span class="num">6.2</span> Algebraic and Transcendental Extensions</div></li>
+                <li class="part"><div><span class="num">6.3</span> Finite Fields</div></li>
+            </ol>
+        </li>
+    </ol>
+
+</aside>
+
+<div class="right">
+</div>
+
 <article>
 	<h1>
 		Logic
@@ -102,17 +166,184 @@
 	:global(body) {
 		background-color: #fefefe;
 		color: #222;
+
+        --nav-width: 260px;
 	}
+
+    :root {
+        --blue: rgb(48, 112, 183);
+    }
+
+    .right {
+        display: none;
+
+        position: fixed;
+        right: 0;
+        top: 0;
+        bottom: 0;
+        width: calc(100vw - 128px - 128px - 700px - var(--nav-width));
+
+        border-left: 1px solid lightgray;
+        background: #f7f7f7;
+
+    }
+
+    .nav {
+        position: fixed;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        width: var(--nav-width);
+
+        overflow: scroll;
+
+        /* border-right: 1px solid lightgray; */
+        padding: 8px;
+        /* padding-right: 16px; */
+
+		/* font-family: 'Inter', sans-serif; */
+		font-family: 'Lora', serif;
+		font-weight: 400;
+		font-style: normal;
+		font-size: 15px;
+		/* line-height: 22px; */
+
+        /* background: #f7f7f7; */
+        /* background: rgb(27, 51, 78); */
+        background: rgb(40, 51, 64);
+        /* color: #ccc; */
+        /* color: rgb(117, 162, 192); */
+        /* color: rgb(129, 174, 204); */
+        color: rgb(138, 173, 196);
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+
+    .nav .section {
+        text-transform: uppercase;
+        /* font-weight: bold; */
+		font-weight: 600;
+        text-align: center;
+        margin-top: 40px;
+        margin-bottom: 8px;
+        /* margin: 0; */
+        /* margin-top: 16px; */
+
+        /* color: #ddd; */
+
+        /* display: grid; */
+        /* grid-template-columns: 1fr auto 1fr; */
+        /* gap: 8px; */
+        /* align-items: center; */
+        /* color: rgb(153, 191, 217); */
+        /* color: rgb(153, 188, 212); */
+        border-bottom: 2px solid rgb(48, 112, 183);
+        border-bottom: 2px solid rgb(60, 106, 158);
+
+        
+    }
+
+    .nav .chapter {
+        /* font-style: italic; */
+        /* color: #ddd; */
+		font-weight: 600;
+    }
+
+    .nav .num {
+        /* font-weight: bold; */
+        /* color: #AAA; */
+        color: rgb(98, 136, 161);
+        color: rgb(103, 136, 159);
+        font-weight: normal;
+        font-variant-numeric: tabular-nums;
+        font-style: normal;
+    }
+
+
+    .nav .section:first-child {
+        margin-top: 16px;
+    }
+    /* .nav .section::before {
+        content: '';
+        display: inline-block;
+        width: 100%;
+        height: 2px;
+        background: rgb(48, 112, 183);
+    } */
+    /* .nav .section::after {
+        content: '';
+        display: inline-block;
+        width: 100%;
+        height: 2px;
+        background: rgb(48, 112, 183);
+    } */
+
+    .nav ol {
+        list-style: none;
+        margin: 0 12px;
+    }
+
+    .nav li {
+        margin: 0;
+        padding: 0;
+        position: relative;
+    }
+
+    .nav li {
+        margin-top: 6px;
+    }
+
+    .nav .chapter-list > li:first-child {
+        /* margin-top: 12px; */
+    }
+    .nav .chapter-list > li:not(:first-child) {
+        margin-top: 16px;
+    }
+
+    .nav .selected::after {
+        display: block;
+        content: '';
+        
+        position: absolute;
+        top: -3px;
+        bottom: -3px;
+        left: -32px;
+        /* height: 100%; */
+        width: calc(var(--nav-width) + 16px);
+        background: #70828b36;
+        z-index: -1;
+    }
+
+    .chapter-num {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+
+		position: absolute;
+		right: 0;
+		bottom: 0;
+
+		background: rgb(48, 112, 183);
+		border-radius: 2px;
+		color: white;
+		width: 80px;
+		height: 80px;
+		font-size: 40px;
+	}
+
 
 	article {
 		font-family: 'Inter', sans-serif;
+		/* font-family: 'Lora', serif; */
 		font-weight: 400;
 		font-style: normal;
 		font-size: 16px;
 		line-height: 25px;
 
 		max-width: 700px;
-		margin-left: 128px;
+		margin-left: calc(128px + var(--nav-width));
 		margin-right: 128px;
 		margin-bottom: 256px;
 		margin-top: 112px;
@@ -142,6 +373,8 @@
 	h2,
 	h3 {
 		font-family: 'Lora', serif;
+        		/* font-family: 'Inter', sans-serif; */
+
 		font-optical-sizing: auto;
 		font-weight: 600;
 		font-style: normal;
@@ -151,24 +384,6 @@
 		justify-content: space-between;
 		width: 100%;
 	}
-
-	.chapter-num {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-
-		position: absolute;
-		right: 0;
-		bottom: 0;
-
-		background: rgb(48, 112, 183);
-		border-radius: 2px;
-		color: white;
-		width: 80px;
-		height: 80px;
-		font-size: 40px;
-	}
-
 	h1 {
 		font-size: 45px;
 		margin-bottom: 32px;
@@ -245,6 +460,7 @@
         color: #7c7f93;
         font-style: italic;
     }    
+
     
 
     :global(.katex) {
@@ -258,3 +474,4 @@
 		border-bottom-width: 1px !important;
 	}
 </style>
+
